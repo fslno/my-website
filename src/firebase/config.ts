91073854cc6bc7dc7c295dc562970be19157cae0
Feisel <fslno.dev@gmail.com>
2026-03-09 06:47:46 +0000

    ulyu5w9XtYeVTmceUfOZLZwDQxF2

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
 'use client';
 
 export const firebaseConfig = {
-  apiKey: "placeholder-api-key",
-  authDomain: "placeholder-project-id.firebaseapp.com",
-  projectId: "placeholder-project-id",
-  storageBucket: "placeholder-project-id.appspot.com",
-  messagingSenderId: "placeholder-sender-id",
-  appId: "placeholder-app-id"
+  apiKey: "AIzaSyCsr2KiSXUIp26muek1a9VR5tpinZHNOoA",
+  authDomain: "studio-1858050787-6ff85.firebaseapp.com",
+  databaseURL: "https://studio-1858050787-6ff85-default-rtdb.firebaseio.com",
+  projectId: "studio-1858050787-6ff85",
+  storageBucket: "studio-1858050787-6ff85.firebasestorage.app",
+  messagingSenderId: "576461835157",
+  appId: "1:576461835157:web:5b3208bccfeaf5aa6b449c"
 };
