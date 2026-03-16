f3950659cdea8432a16eee48049a9376e2f42ab6
Feisel <fslno.dev@gmail.com>
2026-03-16 23:13:32 +0000

    make all the using a drop list at the backend make them to have hovering

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-300 ease-in-out hover:bg-[#D3D3D3] hover:text-[#333333] [&>span]:line-clamp-1",
       className
     )}
     {...props}
