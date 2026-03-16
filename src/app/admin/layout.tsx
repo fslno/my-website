73dc69f6ab6d06d1933ff4a43acd967cd9c7cce7
Feisel <fslno.dev@gmail.com>
2026-03-16 07:06:19 +0000

    Try fixing this error: `Runtime ReferenceError: Badge is not defined. Er

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -52,6 +52,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
+import { Badge } from '@/components/ui/badge';
 import { signOut, signInWithEmailAndPassword } from 'firebase/auth';
 import { useToast } from '@/hooks/use-toast';
 import { doc, collection, query, where, onSnapshot, limit, orderBy } from 'firebase/firestore';
